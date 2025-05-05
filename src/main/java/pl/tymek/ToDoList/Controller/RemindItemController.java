d6fa1ca0f9e1592f986f869312b949f20f6a13f6
Tymoteusz <tymon.pierzchot@gmail.com>
2025-05-05 20:19:29 +0200

    Dodany kontroler

--- src/main/java/pl/tymek/ToDoList/Controller/RemindItemController.java
+++ src/main/java/pl/tymek/ToDoList/Controller/RemindItemController.java
@@ -1,4 +1,18 @@
 package pl.tymek.ToDoList.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import pl.tymek.ToDoList.Service.RemindItemService;
+
+@Controller
 public class RemindItemController {
+    private RemindItemService service;
+
+
+    @GetMapping("/display")
+    public String displayAllItems(Model model){
+        model.addAttribute("remindItems", service.getAllRemindItem());
+        return "display_all";
+    }
 }
